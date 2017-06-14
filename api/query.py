0612943eb8cf8d4fb4e5c733a6c50cfd176618cd
Nicolas Molina Monroy <nicolas.molina.monroy@gmail.com>
2017-06-14 17:54:29 -0400

    save

--- api/query.py
+++ api/query.py
@@ -2,9 +2,10 @@ from graphene import ObjectType, Schema
 from django.contrib.auth.models import User
 
 from api.schemas.user_schema import UsersQuery
-from tasks.query import TasksAppQuery
+from tasks.schemas.category_schema import CategoryQuery
+from tasks.schemas.task_schema import TasksQuery
 
-class Query(TasksAppQuery, UsersQuery,   ObjectType):
+class Query(CategoryQuery, TasksQuery, UsersQuery, ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
